ca73be5315c1f8127329226ddebfe3cf2d3aaa6a
yuya-3110 <ysuu45143745@gmail.com>
2023-03-05 20:54:45 +0900

    Initial Commit

---
+++ Omikuji/OmikujiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmikujiApp.swift
+//  Omikuji
+//
+//  Created by 齊藤有哉 on 2023/03/05.
+//
+
+import SwiftUI
+
+@main
+struct OmikujiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

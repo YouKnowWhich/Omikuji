6f80ecb9da42142a17dda48f982d111331d9bdde
yuya-3110 <ysuu45143745@gmail.com>
2023-03-11 07:25:20 +0900

    modifierの追加

--- Omikuji/ContentView.swift
+++ Omikuji/ContentView.swift
@@ -10,32 +10,49 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
+            Spacer()
             
             // おみくじの数字が0だったら
             if answerNumber == 0 {
                 // 初期画面のテキストを表示
                 Text("今日の運勢は？")
+                    .font(.title)
+                Spacer()
             } else if answerNumber == 1 {
                 // おみくじの数字が1だったら「大吉」
                 Text("大吉")
+                    .font(.title)
+                Spacer()
             } else if answerNumber == 2 {
                 // おみくじの数字が2だったら「吉」
                 Text("吉")
+                    .font(.title)
+                Spacer()
             } else if answerNumber == 3 {
                 // おみくじの数字が3だったら「中吉」
                 Text("中吉")
+                    .font(.title)
+                Spacer()
             } else if answerNumber == 4 {
                 // おみくじの数字が4だったら「小吉」
                 Text("小吉")
+                    .font(.title)
+                Spacer()
             } else if answerNumber == 5 {
                 // おみくじの数字が5だったら「末吉」
                 Text("末吉")
+                    .font(.title)
+                Spacer()
             } else if answerNumber == 6 {
                 // おみくじの数字が6だったら「凶」
                 Text("凶")
+                    .font(.title)
+                Spacer()
             } else {
                 // おみくじの数字が1~6以外だったら「大凶」
                 Text("大凶")
+                    .font(.title)
+                Spacer()
             }
             
             // おみくじの画像
@@ -47,11 +64,16 @@ struct ContentView: View {
             // おみくじボタン
             Button {
                 // ボタンがタップされたときの動き（ランダム）
-                answerNumber = Int.random(in: 0...6)
+                answerNumber = Int.random(in: 1...6)
                 print("タップした！")
             } label: {
                 // ボタンに表示する文字
                 Text("おみくじを引く！")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 70)
+                    .font(.title)
+                    .background(Color.pink)
+                    .foregroundColor(Color.white)
             }
 
         }
